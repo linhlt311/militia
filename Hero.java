d0201c34f8b08cb0171fd027aad93e8e0a88c664
tung <tungts1101@gmail.com>
2018-04-15 18:16:15 +0700

    base clas hero and sword

---
+++ Hero.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+abstract class Hero {
+    protected Position curPosition;
+    protected ArrayList<Position> moveArea;
+    protected ArrayList<Position> attackArea;
+    
+    Hero(Position pos) {
+        curPosition = new Position(pos.getX(), pos.getY());
+        moveArea = new ArrayList<Position> ();
+        attackArea = new ArrayList<Position> ();
+    }
+    
+    void getDetail() {
+        calMoveArea();
+        calAttackArea();
+        System.out.println("Vi tri di chuyen");
+        for(Position pos: moveArea) {
+            System.out.print('('+Integer.toString(pos.getX())+','+pos.getY()+')');
+        }
+        System.out.println();
+        System.out.println("Vi tri tan cong");
+        for(Position pos: attackArea) {
+            System.out.print('('+Integer.toString(pos.getX())+','+pos.getY()+')');
+        }
+    }
+    
+    abstract void calMoveArea();
+    abstract void calAttackArea();
+    abstract boolean move(Position pos);
+    abstract boolean attack(Position pos);
+}
